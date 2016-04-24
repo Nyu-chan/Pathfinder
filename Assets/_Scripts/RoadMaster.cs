c30bde121480ea65d6c9864cfe6bc73b069cb521
Nyu-chan <Nyu-chan@users.noreply.github.com>
2016-04-24 21:24:57 +0200

    Implement GetPath using the A* search algorithm

--- Assets/_Scripts/RoadMaster.cs
+++ Assets/_Scripts/RoadMaster.cs
@@ -1,9 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class RoadMaster : MonoBehaviour
 {
     public List<RoadNode> Nodes;
+
+    private RoadNode target;
+    private SortedList<float, RoadNode> openNodes;
+    private HashSet<RoadNode> closedNodes;
+    private Dictionary<RoadNode, float> distanceFromStart;
+    private Dictionary<RoadNode, RoadNode> predecessors;
 
     // Use this for initialization
     private void Start()
@@ -19,7 +27,87 @@ public class RoadMaster : MonoBehaviour
 
     public List<RoadNode> GetPath(RoadNode nodeFrom, RoadNode nodeTo)
     {
-        return new List<RoadNode>();
+        target = nodeTo;
+        openNodes = new SortedList<float, RoadNode>();
+        closedNodes = new HashSet<RoadNode>();
+        distanceFromStart = new Dictionary<RoadNode, float>();
+        predecessors = new Dictionary<RoadNode, RoadNode>();
+
+        openNodes.Add(0, nodeFrom);
+
+        while (openNodes.Count != 0)
+        {
+            var currentNode = openNodes[0];
+            openNodes.RemoveAt(0);
+
+            if (currentNode == nodeTo)
+                return ReconstructPath();
+
+            closedNodes.Add(currentNode);
+
+            ExpandNode(currentNode);
+        }
+
+        throw new Exception("No path found");
+    }
+
+    private void ExpandNode(RoadNode node)
+    {
+        foreach (var successor in GetSuccessors(node))
+        {
+            if (closedNodes.Contains(successor))
+                continue;
+
+            float oldDistance;
+            distanceFromStart.TryGetValue(node, out oldDistance);
+
+            float newDistance;
+            distanceFromStart.TryGetValue(successor, out newDistance);
+
+            newDistance += 1;
+
+            if (openNodes.ContainsValue(successor) && newDistance >= oldDistance)
+                continue;
+
+            predecessors[successor] = node;
+            distanceFromStart[successor] = newDistance;
+
+            if (openNodes.ContainsValue(successor))
+                openNodes.RemoveAt(openNodes.IndexOfValue(successor));
+
+            var cost = newDistance + CalculateLinearDistance(successor, target);
+            openNodes.Add(cost, successor);
+        }
+    }
+
+    private static float CalculateLinearDistance(RoadNode a, RoadNode b)
+    {
+        return (float)Math.Sqrt(Math.Pow(b.Pos2D.x - a.Pos2D.x, 2) + Math.Pow(b.Pos2D.y - a.Pos2D.y, 2));
+    }
+
+    private List<RoadNode> ReconstructPath()
+    {
+        var path = new List<RoadNode> { target };
+        var current = target;
+        RoadNode predecessor;
+
+        while (predecessors.TryGetValue(current, out predecessor))
+        {
+            path.Add(predecessor);
+            current = predecessor;
+        }
+
+        path.Reverse();
+        return path;
+    }
+
+    private static IEnumerable<RoadNode> GetSuccessors(RoadNode node)
+    {
+        return node.Connections.Select(c =>
+            c.NodeA = node
+                ? c.NodeB
+                : c.NodeA)
+                    .ToList();
     }
 
     public bool IsInNetwork(RoadNode node)
--- Assets/_Scripts/RoadNode.cs
+++ Assets/_Scripts/RoadNode.cs
@@ -1,7 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections.Generic;
 
-public class RoadNode : MonoBehaviour
+public class RoadNode : MonoBehaviour, IEquatable<RoadNode>
 {
     public List<RoadConnection> Connections;
     public Vector2 Pos2D;
@@ -9,17 +10,38 @@ public class RoadNode : MonoBehaviour
     public RoadMaster Master;
 
     // Use this for initialization
-    private void Start () {
-	
-	}
+    private void Start()
+    {
+
+    }
 
     // Update is called once per frame
-    private void Update () {
-	
-	}
+    private void Update()
+    {
+
+    }
 
     public void Init()
     {
         ID = GetInstanceID() + "@" + Time.frameCount;
+    }
+
+    public bool Equals(RoadNode other)
+    {
+        return other != null && other.ID == ID;
+    }
+
+    public override bool Equals(object obj)
+    {
+        var other = obj as RoadNode;
+        return Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return (base.GetHashCode() * 397) ^ (ID != null ? ID.GetHashCode() : 0);
+        }
     }
 }
